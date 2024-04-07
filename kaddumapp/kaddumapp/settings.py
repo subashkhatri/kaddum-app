bb33ac0dfb91d2b1df8315e9938462d118b4f4f4
subash-dai <subash@khatri.com>
2024-04-07 15:14:12 +0930

    - Add dashboard on loader

--- kaddumapp/kaddumapp/settings.py
+++ kaddumapp/kaddumapp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dashboard.apps.DashboardConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'kaddumapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
