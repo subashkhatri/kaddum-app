bed41ce8f3bf3aecac4a0393269a2ff3bd92d269
subash-dai <subash@khatri.com>
2024-04-07 15:13:39 +0930

    - Add methods in view

--- kaddumapp/dashboard/views.py
+++ kaddumapp/dashboard/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+  return render(request, 'index.html')
+
+def dairy_record(request):
+  return render(request, 'dairy_record.html')
