da48905c7d2468209b40974742c984e008175d04
subash-dai <subash@khatri.com>
2024-04-07 15:28:30 +0930

    - Update file structure

--- kaddumapp/dashboard/views.py
+++ kaddumapp/dashboard/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-  return render(request, 'index.html')
+  return render(request, 'dashboard/index.html')
 
 def dairy_record(request):
-  return render(request, 'dairy_record.html')
+  return render(request, 'dashboard/dairy_record.html')
