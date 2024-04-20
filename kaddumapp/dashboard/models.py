994ec84dc6ad6352d273768166067b9762197e5a
subash-dai <subash@khatri.com>
2024-04-20 13:59:23 +0930

    - Update Dairy Record Model

--- kaddumapp/dashboard/models.py
+++ kaddumapp/dashboard/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class DairyRecord(models.Model):
     diary_record_id = models.CharField(max_length=10, primary_key=True)
+    job_name = models.CharField(max_length=255, null=True, blank=True)
+    client = models.CharField(max_length=255, null=True, blank=True)
+    supervisor = models.CharField(max_length=255, null=True, blank=True)
     record_date = models.DateField()
     record_shift = models.CharField(max_length=10)
     activity_discussion = models.TextField(null=True, blank=True)
@@ -13,3 +16,14 @@ class DairyRecord(models.Model):
     handover_note = models.TextField(null=True, blank=True)
     record_created_date = models.DateField(auto_now_add=True)
     record_submitted_date = models.DateField(null=True, blank=True, auto_now=True)
+
+    # New numeric fields
+    mobilised = models.IntegerField(null=True, blank=True)
+    non_manual = models.IntegerField(null=True, blank=True)
+    manual = models.IntegerField(null=True, blank=True)
+    subcontractor = models.IntegerField(null=True, blank=True)
+    environmental_incidents = models.IntegerField(null=True, blank=True)
+    near_misses = models.IntegerField(null=True, blank=True)
+    first_aid = models.IntegerField(null=True, blank=True)
+    medically_treated_injury = models.IntegerField(null=True, blank=True)
+    loss_time_injury = models.IntegerField(null=True, blank=True)
