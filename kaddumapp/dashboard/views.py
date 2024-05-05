99af9cdc1ecd768937a4b129ae8b49dfcb940b94
subash-dai <subash@khatri.com>
2024-05-05 14:34:14 +0930

    - Refactor my create method for dairy record - Rename record_create_date field to record_date

--- kaddumapp/dashboard/views.py
+++ kaddumapp/dashboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
+from .dairy_record_forms import DairyRecordForm
 from .models import DairyRecord, CostTracking, Project, DayTracking, DayTrackingEmployeeDetails, ResourceCost
 from users.models import UserAccount
 from django.utils.dateparse import parse_date
@@ -12,7 +13,7 @@ import datetime
 logger = logging.getLogger(__name__)
 
 def index(request):
-  dairy_records = DairyRecord.objects.order_by('-record_created_date')
+  dairy_records = DairyRecord.objects.order_by('-created_date')
   return render(request, 'dashboard/index.html', {'records': dairy_records})
 
 def dairy_record(request):
@@ -25,48 +26,15 @@ def all_dairy_record(request):
 def create_dairy_record(request):
     form_action = reverse('create_dairy_record')
     if request.method == 'POST':
-        form_data = request.POST
-        try:
-            record_date = parse_date(form_data.get('record_date')) if form_data.get('record_date') else None
-            new_record = DairyRecord(
-                job_name=form_data.get('job_name', ''),
-                client=form_data.get('client', ''),
-                supervisor=form_data.get('supervisor', ''),
-                record_date=record_date,
-                record_shift=form_data.get('record_shift', ''),
-                activity_discussion=form_data.get('activities_discussed', ''),
-                safety_issue_discussion=form_data.get('safety_issues_discussed', ''),
-                instruction_from_client=form_data.get('instruction_received', ''),
-                visitor_on_site=form_data.get('site_visitor', ''),
-                daily_progress_description=form_data.get('work_description', ''),
-                record_comment=form_data.get('notes', ''),
-                handover_note=form_data.get('handover_notes', ''),
-                mobilised=int(form_data.get('mobilised', 0)),
-                non_manual=int(form_data.get('non_manual', 0)),
-                manual=int(form_data.get('manual', 0)),
-                subcontractor=int(form_data.get('sub_contractor', 0)),
-                environmental_incidents=int(form_data.get('environmental_incidents', 0)),
-                near_misses=int(form_data.get('near_misses', 0)),
-                first_aid=int(form_data.get('first_aid', 0)),
-                medically_treated_injury=int(form_data.get('medically_treated_injury', 0)),
-                loss_time_injury=int(form_data.get('loss_time_injury', 0)),
-                record_created_date=date.today()
-            )
-            new_record.save()
-            messages.success(request, 'Dairy record created successfully')
-            return redirect('index')
-        except Exception as e:
-            logger.error('Error saving form data:', exc_info=True)
-            messages.error(request, 'Error creating dairy record: {}'.format(e))
-            return render(request, 'dashboard/create_dairy_record.html', {
-                'form_action': form_action,
-                'dairy_record': None
-            }, status=400)
+        form = DairyRecordForm(request.POST)
+        if form.is_valid():
+          form.save()
+          messages.success(request, 'Dairy record created successfully.')
+          return redirect('index')
     else:
-         return render(request, 'dashboard/create_dairy_record.html', {
-            'form_action': form_action,
-            'dairy_record': None
-        })
+        form = DairyRecordForm()
+        projects = Project.objects.all()  # Assuming Project model has all projects
+        return render(request, 'dashboard/create_dairy_record.html', {'form': form, 'projects': projects, 'form_action': form_action})
 
 def edit_dairy_record(request, dairy_record_id):
     dairy_record = get_object_or_404(DairyRecord, id=dairy_record_id)
@@ -94,7 +62,7 @@ def edit_dairy_record(request, dairy_record_id):
             dairy_record.first_aid = int(form_data.get('first_aid', 0))
             dairy_record.medically_treated_injury = int(form_data.get('medically_treated_injury', 0))
             dairy_record.loss_time_injury = int(form_data.get('loss_time_injury', 0))
-            dairy_record.record_created_date = date.today()
+            dairy_record.created_date = date.today()
 
             dairy_record.save()
             messages.success(request, 'Dairy record updated successfully')
@@ -119,8 +87,9 @@ def daily_costing(request):
     })
 
 def all_daily_costing(request):
-    daily_costing = CostTracking.objects.order_by('-record_created_date')
+    daily_costing = CostTracking.objects.order_by('-created_date')
     return render(request, 'costing/all_daily_costing.html', {'records': daily_costing})
+
 
 def check_day_tracking(request):
     project_name = request.GET.get('projectName')
