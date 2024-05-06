c8f74acfc34a315fcc5ecfc4510073f65c8ff1b7
kkkkabi <sinmei_li@hotmail.com>
2024-05-07 01:27:36 +0930

    update dairy record pages UI for consistence

--- kaddumapp/dashboard/views_dairy_record.py
+++ kaddumapp/dashboard/views_dairy_record.py
@@ -10,18 +10,22 @@ def view_dairy_record(request, dairy_record_id):
     return render(request, 'dairy_record/view_dairy_record.html', {'record': record})
 
 def all_dairy_record(request):
-    records_list = DairyRecord.objects.order_by('-created_date')
-    paginator = Paginator(records_list, 10)  # Show 10 records per page.
+    draft_records_list = DairyRecord.objects.filter(is_draft=True).order_by('-created_date')
+    completed_records_list = DairyRecord.objects.filter(is_draft=False).order_by('-created_date')
+    paginator = Paginator(completed_records_list, 10)  # Show 10 records per page.
     page_number = request.GET.get('page')
     dairy_records = paginator.get_page(page_number)
-    return render(request, 'dairy_record/all_dairy_record.html', {'records': dairy_records})
+    return render(request, 'dairy_record/all_dairy_record.html', {'draft_records': draft_records_list, 'completed_records':completed_records_list})
 
 def create_dairy_record(request):
     form_action = reverse('create_dairy_record')
     if request.method == 'POST':
         form = DairyRecordForm(request.POST)
         if form.is_valid():
-          form.save()
+          is_draft = request.POST.get('click-btn') == 'draft'
+          dairy_record = form.save(commit=False)
+          dairy_record.is_draft = is_draft
+          dairy_record.save()
           messages.success(request, 'Dairy record created successfully.')
           return redirect('index')
     else:
