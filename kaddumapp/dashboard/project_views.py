7a3dab068aa86e6108d6a0bfba0a3050b68d2102
kkkkabi <sinmei_li@hotmail.com>
2024-05-06 11:26:15 +0930

    feature/project_CRUD page

---
+++ kaddumapp/dashboard/project_views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from .project_forms import ProjectForm
+from .models import Project
+from django.contrib import messages
+from .project_forms import ProjectForm
+import logging
+from django.core.paginator import Paginator
+
+
+
+logger = logging.getLogger(__name__)
+
+def project_list(request):
+    records_list = Project.objects.order_by('-created_date')
+    paginator = Paginator(records_list, 10)  # Show 10 records per page.
+    page_number = request.GET.get('page')
+    projects = paginator.get_page(page_number)
+    return render(request, 'project/project_list.html', {'records': projects})
+
+def project_create(request):
+    form_action = reverse('project_create')
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+          form.save()
+          messages.success(request, 'Project created successfully.')
+          return redirect('project_list')
+    else:
+        form = ProjectForm()
+        return render(request, 'project/project_create.html', {'form': form, 'form_action': form_action})
+    
+
+def project_update(request, project_no):
+    form_action = reverse('project_update', args=[project_no])
+    record = get_object_or_404(Project, pk=project_no)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=record)
+        if form.is_valid():
+          form.save()
+          messages.success(request, 'Project updated successfully.')
+          return redirect('project_list')
+    else:
+        form = ProjectForm(instance=record)
+        return render(request, 'project/project_update.html', {'form': form, 'record': record, 'form_action': form_action})
+
+def project_delete(request, project_no):
+    record = get_object_or_404(Project, pk=project_no)
+
+    if request.method == 'POST':
+        record.delete()
+        messages.success(request, "Project deleted successfully.")
+        return redirect('project_list')
+
+    return render(request, 'project/delete_confirm.html', {'record': record})
+
+
--- kaddumapp/dashboard/urls.py
+++ kaddumapp/dashboard/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from . import views
+from . import views, project_views
+
 
 urlpatterns = [
   path('', views.index, name='index'),
@@ -8,6 +9,13 @@ urlpatterns = [
   path('all_dairy_records', views.all_dairy_record, name='all_dairy_record'),
   path('view_dairy_record/<str:dairy_record_id>/', views.view_dairy_record, name="view_dairy_record"),
   path('delete_dairy_record/<str:dairy_record_id>/', views.delete_dairy_record, name='delete_dairy_record'),
+
   path('daily_costing', views.daily_costing, name='daily_costing'),
   path('all_daily_costing', views.all_daily_costing, name='all_daily_costing'),
+
+  path('project_create/', project_views.project_create, name='project_create'),
+  path('project_update/<str:project_no>/', project_views.project_update, name='project_update'),
+  path('project_list', project_views.project_list, name='project_list'),
+  path('project_delete/<str:project_no>/', project_views.project_delete, name='project_delete'),
+  # path('project_view/<str:project_no>/', project_views.view_dairy_record, name="project_view"),  to be done
 ]
