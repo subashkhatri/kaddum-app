3cf8e80e7c883a172b7d68e7b372a3a4094e4510
kkkkabi <sinmei_li@hotmail.com>
2024-05-07 21:13:09 +0930

    complete project pages and tidy all pages

--- kaddumapp/dashboard/views_dairy_record.py
+++ kaddumapp/dashboard/views_dairy_record.py
@@ -9,7 +9,7 @@ def view_dairy_record(request, dairy_record_id):
     record = get_object_or_404(DairyRecord, pk=dairy_record_id)
     return render(request, 'dairy_record/view_dairy_record.html', {'record': record})
 
-def all_dairy_record(request):
+def all_dairy_records(request):
     draft_records_list = DairyRecord.objects.filter(is_draft=True).order_by('-created_date')
     completed_records_list = DairyRecord.objects.filter(is_draft=False).order_by('-created_date')
     paginator = Paginator(completed_records_list, 10)  # Show 10 records per page.
@@ -27,7 +27,7 @@ def create_dairy_record(request):
           dairy_record.is_draft = is_draft
           dairy_record.save()
           messages.success(request, 'Dairy record created successfully.')
-          return redirect('index')
+          return redirect('all_dairy_records')
     else:
         form = DairyRecordForm()
         return render(request, 'dairy_record/create_dairy_record.html', {'form': form, 'form_action': form_action})
@@ -40,7 +40,7 @@ def edit_dairy_record(request, dairy_record_id):
         if form.is_valid():
           form.save()
           messages.success(request, 'Dairy record updated successfully.')
-          return redirect('index')
+          return redirect('all_dairy_records')
     else:
         form = DairyRecordForm(instance=record)
         return render(request, 'dairy_record/edit_dairy_record.html', {'form': form, 'dairy_record': record, 'form_action': form_action})
@@ -51,6 +51,6 @@ def delete_dairy_record(request, dairy_record_id):
     if request.method == 'POST':
         record.delete()
         messages.success(request, "Dairy record deleted successfully.")
-        return redirect('all_dairy_record')
+        return redirect('all_dairy_records')
 
     return render(request, 'dairy_record/confirm_delete.html', {'record': record})
