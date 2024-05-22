89fd1bdf26ba02265e7b2de743cdc45d2bf55119
Anup-kadariya <53438855+Anup-kadariya@users.noreply.github.com>
2024-05-22 21:53:26 +0930

    Getting draft history on front page

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
-from .models import DairyRecord
+from .models import DairyRecord, DayTracking
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='login')
 def index(request):
-  dairy_records = DairyRecord.objects.order_by('-created_date')[:10]
-  return render(request, 'dashboard/index.html', {'records': dairy_records})
+  dairy_records = DairyRecord.objects.filter(is_draft=True).order_by('-created_date')[:5]
+  day_trackings = DayTracking.objects.filter(is_draft=True).order_by('-created_date')[:5]
+  return render(request, 'dashboard/index.html', {'dairy_records': dairy_records, 'day_trackings': day_trackings})
 
 def not_found(request, exception):
    return redirect('index')
